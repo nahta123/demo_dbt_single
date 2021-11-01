850e6ce2b2eec734a8f2d2336444e910825e818c
Nikhil Jain <nikhil.jain@dotpe.in>
2021-11-01 10:01:14 +0530

    single model with single node

---
+++ models/example/my_first_dbt_model.sql
@@ -0,0 +1,20 @@
+-- {{ config(
+--     schema='partitions'
+--     ) }}
+
+
+-- select * from RAW.AGGREGATOR.OPTION_VARIANTS
+-- where _AIRBYTE_EMITTED_AT > (select max(_AIRBYTE_EMITTED_AT) from {{this}})
+
+{{ config(
+    schema='partitions',
+    materialized='incremental'
+    ) }}
+
+
+select * from RAW.AGGREGATOR.OPTION_VARIANTS
+{% if is_incremental() %}
+
+  where _AIRBYTE_EMITTED_AT > (select max(_AIRBYTE_EMITTED_AT) from {{this}})
+
+{% endif %}
